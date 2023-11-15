fe92ddd4e7d1834be197d65dd8d281eb15563d01
echatv <Evan.Chatters@votegrity.net>
2023-11-15 01:49:53 -0700

    Copied from other repo

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,17 @@
 #include <GLFW/glfw3.h>
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
-
+#include <glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
 #include <cstdio>
 #include <exception>
 #include <iostream>
 #include <sstream>
 
 #include "imgui_impl_opengl3.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 namespace
 {
@@ -76,6 +80,49 @@ namespace
   }
 } // namespace
 
+// Square corrdinates
+float vertices[] = {
+    // positions          // colors           // texture coords
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+};
+unsigned int indices[] = {  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3,    // second triangle
+};
+
+
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"layout (location = 2) in vec2 aTexCoord;\n"
+
+"out vec3 ourColor;\n"
+"out vec2 TexCoord;\n"
+
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   ourColor = aColor;\n"
+"   TexCoord = aTexCoord;\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+
+"in vec3 ourColor;\n"
+"in vec2 TexCoord;\n"
+
+"uniform sampler2D ourTexture;\n"
+
+"void main()\n"
+"{\n"
+"   FragColor = texture(ourTexture, TexCoord);\n"
+"}\0";
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] const char* const* argv)
 {
   ZoneScoped; // Tells Tracy to profile this scope
@@ -91,11 +138,11 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char* const* argv)
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_MAXIMIZED, GLFW_FALSE);
   glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
   glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
   glfwWindowHint(GLFW_SRGB_CAPABLE, GLFW_TRUE);
   glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+  glfwWindowHint(GLFW_MAXIMIZED, GL_TRUE);
 
   // Create a monitor that is 0.75x0.75 the size 
   GLFWmonitor* monitor = glfwGetPrimaryMonitor();
@@ -104,7 +151,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char* const* argv)
     throw std::runtime_error("No monitor detected");
   }
   const GLFWvidmode* videoMode = glfwGetVideoMode(monitor);
-  GLFWwindow* window = glfwCreateWindow(static_cast<int>(videoMode->width * .75), static_cast<int>(videoMode->height * .75), "bababooey", nullptr, nullptr);
+  GLFWwindow* window = glfwCreateWindow(static_cast<int>(videoMode->width), static_cast<int>(videoMode->height), "bababooey", nullptr, nullptr);
   if (!window)
   {
     glfwTerminate();
@@ -122,6 +169,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char* const* argv)
   
   glfwMakeContextCurrent(window);
   glfwSwapInterval(1); // 1 = vsync, 0 = no vsync
+
+  glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(videoMode->width), static_cast<float>(videoMode->height), 0.0f, -1.0f, 1.0f);
 
   // Load OpenGL function pointers
   int version = gladLoadGL(glfwGetProcAddress);
@@ -145,6 +194,97 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char* const* argv)
   ImGui::StyleColorsDark();
   ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
+  // Vertex buffer object
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+
+  // Vertex shader
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  // Error checking
+  int  success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+  if (!success)
+  {
+      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+
+  // Fragment shader
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  // Error checking
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+      glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+  }
+
+  glUseProgram(shaderProgram);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  unsigned int EBO;
+  glGenBuffers(1, &EBO);
+
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  // ..:: Initialization code (done once (unless your object frequently changes)) :: ..
+  // 1. bind Vertex Array Object
+  glBindVertexArray(VAO);
+  // 2. copy our vertices array in a buffer for OpenGL to use
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  // 3. copy our index array in a element buffer for OpenGL to use
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  // 4. then set the vertex attributes pointers
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  // Load Texture
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  // set the texture wrapping/filtering options (on the currently bound texture object)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  // load and generate the texture
+  int width, height, nrChannels;
+  stbi_set_flip_vertically_on_load(true);
+  unsigned char* data = stbi_load("models/Sprite-0002.png", &width, &height, &nrChannels, 4);
+  if (data)
+  {
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else
+  {
+      std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+
+  //Game loop
   while(!glfwWindowShouldClose(window))
   {
     // Profile the main loop
@@ -162,7 +302,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char* const* argv)
     }
 
     // Clear the window
-    glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
+    glClearColor(1.0f, 0.0f, 0.5f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     // Create a window with ImGui and put some text in it
@@ -174,8 +314,20 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char* const* argv)
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
+    // ..:: Drawing code (in render loop) :: ..
+    // 4. draw the object
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(shaderProgram);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+
     glfwSwapBuffers(window);
   }
+
+
 
   // Clean up after ourselves
   ImGui_ImplOpenGL3_Shutdown();
